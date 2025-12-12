5035288d1a0e74ecfc44443b44f973833894cf4f
Omeiza-ux <aliyuvictor78@gmail.com>
2025-12-12 10:43:23 +0100

    12/12/2025

---
+++ class.js
@@ -0,0 +1,23 @@
+/*const number1 = 5;
+const number2 =3;
+const total = number1 + number2;
+
+ if (total > 7) {
+    console.log("greater that seven");
+ } else (total < 7) {
+    console.log("less than seven");
+ }*/
+
+const number1 = 5;
+const number2 = 3;
+const total = number1 + number2;
+
+function checkGreaterThan() {
+  if (total > 7) {
+    console.log('greater that seven');
+  } else {
+    console.log('less than seven');
+  }
+}
+
+checkGreaterThan();
